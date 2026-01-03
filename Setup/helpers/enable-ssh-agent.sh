c2de92224ac1ce633202a16f87e281a46f340347
jl115 <joelbern006@gmail.com>
2026-01-03 14:36:55 +0100

    fix: changed premission of keys to only this user

--- Setup/helpers/enable-ssh-agent.sh
+++ Setup/helpers/enable-ssh-agent.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-SSH_HOME="~/.ssh"
+SSH_HOME="$HOME/.ssh"
+
+echo "🔒 Fixing SSH permissions..."
+
+chmod 600 "$SSH_HOME/bitbucket-ssh"
+chmod 600 "$SSH_HOME/github"
+chmod 600 "$SSH_HOME/id_rsa"
+
+chmod 644 "$SSH_HOME/"*.pub
+chmod 644 "$SSH_HOME/config"
+chmod 644 "$SSH_HOME/known_hosts" 2>/dev/null
 
 echo "🔑 Starting SSH Agent..."
 eval "$(ssh-agent -s)"
 
 echo "➕ Adding keys to agent..."
-# --apple-use-keychain stores the passphrase in the macOS Keychain so you don't re-type it
 ssh-add --apple-use-keychain "$SSH_HOME/bitbucket-ssh"
 ssh-add --apple-use-keychain "$SSH_HOME/github"
 ssh-add --apple-use-keychain "$SSH_HOME/id_rsa"
